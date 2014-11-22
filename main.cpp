ae629b040918c3a5adf3d65d6d9db06f76a97f63
Pascal WIchmann <wichmannpas@gmail.com>
2014-11-22 13:42:50 +0100

    feature: basic server

--- main.cpp
+++ main.cpp
@@ -1,8 +1,11 @@
 #include <QCoreApplication>
+#include "server.h"
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
+
+    server serverInstance;
 
     return a.exec();
 }
