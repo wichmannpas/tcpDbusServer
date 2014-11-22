70f7ed51dbb54e73c779b64044e4c408e5f7701b
Pascal WIchmann <wichmannpas@gmail.com>
2014-11-22 14:04:47 +0100

    feature: server class receives tcp messages and prints them

--- server.cpp
+++ server.cpp
@@ -1,5 +1,39 @@
-#include "server.h"
+/*
+ * Written by Pascal Wichmann
+ */
 
-server::server()
+#include "server.h"
+#include <iostream>
+
+server::server(QObject* parent): QObject(parent)
 {
+    connect(&tcpServer, SIGNAL(newConnection()), this, SLOT(handleConnection()));
+    tcpServer.listen(QHostAddress::Any, 12341);//listen on tcp port 12341
+}
+
+server::~server()
+{
+    tcpServer.close();
+}
+
+/*
+ * handle incoming tcp connection
+ */
+void server::handleConnection()
+{
+    tcpClient = tcpServer.nextPendingConnection();//get tcp connection
+    connect(tcpClient, SIGNAL(readyRead()), this, SLOT(readFromConnection()));//read data from tcp connection
+}
+
+/*
+ * read data from tcp connection
+ */
+void server::readFromConnection()
+{
+    char buffer[1024];
+    tcpClient->read(buffer, tcpClient->bytesAvailable());//write message to buffer
+
+    std::cout << "Received tcp message: " << QString(buffer).toStdString() << std::endl;//print tcp message with cout
+
+    tcpClient->close();//close the tcp connection to client
 }
--- server.h
+++ server.h
@@ -1,10 +1,29 @@
+/*
+ * Written by Pascal Wichmann
+ */
+
 #ifndef SERVER_H
 #define SERVER_H
 
-class server
+#include <QObject>
+#include <QtNetwork>
+#include <QTcpServer>
+#include <QTcpSocket>
+
+class server: public QObject
 {
+Q_OBJECT
 public:
-    server();
+    server(QObject * parent = 0);
+    ~server();
+
+public slots:
+    void handleConnection();
+    void readFromConnection();
+
+private:
+    QTcpServer tcpServer;
+    QTcpSocket* tcpClient;
 };
 
 #endif // SERVER_H
